5e6ba6fc5416fa164ef85a1c829e882459eb3861
Daniel <danielkim2711@gmail.com>
2022-01-21 17:13:17 +1300

    Add main functionality

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const gridContainer = document.querySelector('.grid-container');
+const blackBtn = document.querySelector('.black');
+const rainbowBtn = document.querySelector('.rainbow');
+const clearBtn = document.querySelector('.clear');
+
+const square = document.querySelector('div');
+const squares = square.childNodes;
+
+function createGrid() {
+  const numberOfSqaures = 16;
+
+  for (let i = 1; i <= numberOfSqaures ** 2; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('style', 'border: 1px solid #000;');
+    gridContainer.appendChild(div);
+  }
+
+  gridContainer.setAttribute(
+    'style',
+    `grid-template-columns: repeat(${numberOfSqaures}, 1fr); grid-template-rows: repeat(${numberOfSqaures}, 1fr);`
+  );
+}
+
+function drawBlack() {
+  squares.forEach((square) =>
+    square.addEventListener('mouseover', (e) => {
+      e.target.setAttribute(
+        'style',
+        'background-color: #000; border: 1px solid #fff'
+      );
+    })
+  );
+}
+
+function drawRainbow() {
+  squares.forEach((square) =>
+    square.addEventListener('mouseover', (e) => {
+      e.target.setAttribute(
+        'style',
+        `background-color: rgb(${Math.floor(
+          Math.random() * 255 + 1
+        )},${Math.floor(Math.random() * 255 + 1)},${Math.floor(
+          Math.random() * 255 + 1
+        )}); border: 1px solid #fff`
+      );
+    })
+  );
+}
+
+blackBtn.addEventListener('click', drawBlack);
+rainbowBtn.addEventListener('click', drawRainbow);
+
+clearBtn.addEventListener('click', () =>
+  squares.forEach((square) => {
+    square.setAttribute(
+      'style',
+      'background-color: #fff; border: 1px solid #000'
+    );
+  })
+);
+
+createGrid();
